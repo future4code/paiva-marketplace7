e3ba17a576c355a8ae399778ba65d74314cd38a5
hgalvao98 <henrique.galvao@hotmail.com>
2021-05-27 09:55:36 -0300

    paginadeserviços

--- src/components/pages/NinjaPage/Styled.js
+++ src/components/pages/NinjaPage/Styled.js
@@ -1,1 +1,70 @@
 import styled from "styled-components";
+
+
+export const Main = styled.div`
+    width:100vw;
+    height:100vh;
+    display:flex;
+    flex-direction:row;
+` 
+
+export const NinjaForm = styled.div`
+    padding:8px 24px 24px 24px;
+    h1{
+        font-size:100px;
+    }
+    width:200%;
+    height:100%;
+    display:flex;
+    flex-direction:column;
+    background-color:#F5F4FC;
+    label{
+        margin:8px;
+    }
+    input{
+        margin:8px;
+        border-radius:10px;
+    }
+    textarea{
+        border-radius:10px;
+        margin:8px;
+        height:100px;
+        justify-content:flex-start;
+        vertical-align:top;
+    }
+    select{
+        margin:8px;
+        border-radius:10px;
+    }
+` 
+
+export const NinjaCart = styled.div`
+    padding:4px;
+    width:100vw;
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    border-left:2px solid lightgray;
+    background-color:#F5F4FC;
+    h2{
+        font-size:28px;
+        padding-top:64px;
+        margin-bottom:60px;
+    }
+`
+export const Lista = styled.p`
+    padding:8px;
+
+`
+
+export const ListaContainer = styled.div`
+    display:flex;
+    justify-content:space-between;
+    padding:8px;
+    width:100%;
+    border-bottom:1px solid lightgray;
+    button{
+        background
+    }
+`
