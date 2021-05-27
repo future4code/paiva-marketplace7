d5238aab8f7866421123405dd85cf45951496687
SilasPaixao <silas.paixao873@gmail.com>
2021-05-27 14:59:59 -0300

    Layout da pag Contratante stagio 1

--- src/components/pages/UserPage/Styled.js
+++ src/components/pages/UserPage/Styled.js
@@ -1,1 +1,33 @@
 import styled from "styled-components";
+
+export const Filtro = styled.div`
+    display:flex;
+    justify-content: space-between;
+    align-items:center;
+    padding:4px;
+    margin-bottom:15px;
+    input{
+        border:1px solid #7660C3;
+        padding:3px;
+        border-radius:5px;
+    }
+    select{
+        background-color:#eee;
+        color: gray;
+        padding:3px;
+    }
+`
+export const Conteiner=styled.div`
+    display:flex;
+    justify-content: space-between;
+    align-items:center;
+    div{
+        height:65vh;  //SERÁ APAGADO PARA ENTRADA DOS DADOS NESTE CAMPO
+    }
+`
+export const Title=styled.p`
+    text-align:center;
+    display:inline-block;
+    width: 40vw;
+    padding:6px;
+`
--- src/components/pages/UserPage/UserPage.js
+++ src/components/pages/UserPage/UserPage.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import Header from '../../Header/Header';
+import Footer from '../../Footer/Footer';
+import {Filtro, Conteiner, Title} from './Styled';
+import { Container } from '@chakra-ui/layout';
+
 
 export default class UserPage extends React.Component{
+    state={
+        produtos:[],
+        tipoService:"all",
+        min:0,
+        max:10000,
+        ordem:"min-max",
+        busca:""
 
+    }
+/*
+    getServices = ()=>{
+
+    }
+    Filter = ()=>{
+        
+    }
+    ordenar = ()=>{
+        
+    }
+    addAoCarrinho = ()=>{
+        
+    }
+    removerDoCarrinho = ()=>{
+        
+    }
+    cancelarTodosServices = ()=>{
+        
+    }
+    solicitarECancelarServices = ()=>{
+        
+    }
+
+*/
     ninjaPage = () =>{
         this.props.choosePage("ninja")
     }
@@ -11,10 +47,40 @@ export default class UserPage extends React.Component{
         return(
             <div>
                 <Header texto={'Seja um Ninja'} troca={this.ninjaPage}/>
-                User Page
+                <Filtro>
+                    <input placeholder="Nome do serviço" />
+                    <select>
+                        <option>Tipo de serviço</option>
+                        <option>Todos</option>
+                    </select>
+                    <input placeholder="Valor mínimo" />
+                    <input placeholder="Valor máximo" />
+                    <select>
+                        <option>Ordenar por</option>
+                        <option>Menor preço</option>
+                        <option>Maior preço</option>
+                    </select>
+                </Filtro>
+                <Conteiner>
+                    <div>
+                        <Title>Lista de Serviços</Title>
+                        <hr />
+                        <br />
+                        {/*AQUI VAI O CARDSERVICES*/}
+                    </div>
+
+                    <div>
+                        <Title>Carrinho</Title>
+                        <hr />
+                        <br />
+                        {/*AQUI VAI O CARRINHO*/}
+                    </div>
+                </Conteiner>
+               
+                <Footer />
             </div>
-        )
-            
+        
+        )    
 
     }
 }
